962910fe3a511407c093ba62ae89073bd7cf681e
Jose Martinez <josemartinez@Joses-Air.lan>
2023-07-02 00:26:07 -0700

    pets will now have their own profiles

--- petfax/pet.py
+++ petfax/pet.py
@@ -3,9 +3,18 @@ import json
 
 bp = Blueprint('pet', __name__, url_prefix="/pets")
 pets = json.load(open("pets.json"))
-print(pets)
+# print(pets)
 
 
 @bp.route('/')
 def index(): 
     return render_template("index.html", pets=pets)
+
+@bp.route('/profile/<id>')
+def animal_profile(id):
+    for pet in pets:
+        #so for some reason parameters are a String datatype by default ("bruh")
+        if pet['pet_id'] == int(id):
+            return render_template("profile.html", pet = pet )
+
+            
